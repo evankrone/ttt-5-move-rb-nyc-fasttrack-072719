eb90d3cf04a5a065e68bfda7b0bdb563f7557769
boolean-messaging-0131 <evankrone1@gmail.com>
2019-07-28 17:43:12 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+ board_array = ["0","1", "2", "3", "4","5", "6", "7", "8"]
+
 # code your input_to_index and move method here!
+
+def user_input
+  if 
+    "5"
